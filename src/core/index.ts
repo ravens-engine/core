2d97441de08ed62a592e1214d6e24a9a6f9c402a
Mark Klara <mrhappyasthma@gmail.com>
2021-10-10 10:38:40 -0700

    Expose `GameStatus` in `index.ts`.
    I'm unable to import `GameStatus` from the `@ravens-engine/core/lib/core/index.js`.
    
    I am able to do:
    
    ```
    import { GameStatus } from "@ravens-engine/core/lib/core/GameStatus.js";
    ```
    
    However it seems like it would be convenient to expose it from this umbrella import `index.ts`.

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,6 @@
 export * from "./Core";
 export * from "./Game";
+export * from "./GameStatus";
 export * from "./Phase";
 export * from "./errors/InvalidActionError";
 export * from "./errors/InvalidStatusError";
